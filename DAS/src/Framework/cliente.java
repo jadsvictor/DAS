612682ab15ad5a759eba2a45c702c6e27a718b80
Jads <jadsvictor@hotmail.com>
2013-12-13 18:09:32 -0200

    classe de Teste "cliente" pronta

--- DAS/src/Framework/cliente.java
+++ DAS/src/Framework/cliente.java
@@ -1,11 +1,25 @@
 package Framework;
 
 import Recurso.RecursoFabricaConcreta;
-
+import java.util.Scanner;
+	 
 public class cliente {
-
-	public static void main (String[] args){
-		
+	 
+	   public static void main(String[] args){
+	         
+	        // Create the factory object
+	        RecursoFabricaConcreta fabrica = new RecursoFabricaConcreta();
+	         
+	        // criando produto abstrato         
+	        RecursoProdutoAbstrato produto = null;        
+	        Scanner userInput = new Scanner(System.in);
+	                  
+	            int tipoRecurso = 1;
+	         
+            produto = fabrica.factoryMethod(tipoRecurso);
+	    System.out.print( tipoRecurso+ ":  " + produto);      
           
-}
-}
+	           
+	         
+	        }
+           }
