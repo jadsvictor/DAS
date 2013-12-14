115611e0dfec612fe7b436dd7afc198028328401
Jads <jadsvictor@hotmail.com>
2013-12-14 16:57:20 -0200

    criando classe fachada

---
+++ DAS/src/Framework/FachadaRecurso.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Framework;
+
+/**
+ *
+ * @author jads
+ */
+public class FachadaRecurso {
+    
+}
