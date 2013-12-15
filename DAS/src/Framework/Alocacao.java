4b3883b0faad14f90ce9fcd35514b76508d3ffb3
Jads <jadsvictor@hotmail.com>
2013-12-15 14:52:10 -0200

    criar alocação / alocar()

--- DAS/src/Framework/Alocacao.java
+++ DAS/src/Framework/Alocacao.java
@@ -3,71 +3,110 @@ package Framework;
 import Recurso.RecursoFabricaConcreta;
 import Recurso.RecursoProdutoConcreto;
 import Usuario.UsuarioProdutoConcreto;
+import Usuario.UsuarioFabricaConcreta;
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import javax.swing.JOptionPane;
 
 public class Alocacao {
   
- static ArrayList<Alocacao> alocacao = new ArrayList<Alocacao> ();  
+ static ArrayList<Alocacao> alocacoes = new ArrayList<Alocacao> ();  
     
     String recurso;
     String usuario;
-    Date dataInicio;
-    Date dataFim;
-    String horaInicio;
-    String horaFim;
+    String alocarData;
 
 public void alocar (){
     
-    RecursoFabricaConcreta fabricaconcreta = new RecursoFabricaConcreta();
+    Alocacao alocacao= null;
     String numeroPatrimonioAlocar= null;
-    numeroPatrimonioAlocar = JOptionPane.showInputDialog("Informe o numero do patrimonio do recurso que deseja alocar");
-        for (int i = 0; i < fabricaconcreta.getRecursosCadastrados().size(); i++) { 
-             if (fabricaconcreta.getRecursosCadastrados().get(i).numeroPatrimonio.equals(numeroPatrimonioAlocar)){
-                 recurso = numeroPatrimonioAlocar;
-             }
-        }    
-}
+    String matriculaUsuarioAlocar= null;
+    boolean achouRecurso = false;
+    boolean achouUsuario = false ;
     
-    public Alocacao(Date dataInicio, Date dataFim, String horaInicio, String horaFim) {
-        this.dataInicio = dataInicio;
-        this.dataFim = dataFim;
-        this.horaInicio = horaInicio;
-        this.horaFim = horaFim;
+    //pesquisando recurso 
+    RecursoFabricaConcreta fabricaconcreta = new RecursoFabricaConcreta();
+    numeroPatrimonioAlocar = JOptionPane.showInputDialog("Informe o numero do patrimonio do recurso que deseja alocar");
+        for (int i = 0; i < fabricaconcreta.getRecursosCadastrados().size() || achouRecurso == false ; i++) { 
+             if (fabricaconcreta.getRecursosCadastrados().get(i).numeroPatrimonio.equals(numeroPatrimonioAlocar)){
+                 alocacao.setRecurso(numeroPatrimonioAlocar);
+                 achouRecurso = true;
+             }
+        }
+        if (achouRecurso==false){
+            JOptionPane.showMessageDialog(null, "Numero do patrimonio nao encontrado");
+        }
+        
+    //pesquisando usuario
+    UsuarioFabricaConcreta fabricaconcretaU = new UsuarioFabricaConcreta();
+    matriculaUsuarioAlocar = JOptionPane.showInputDialog("Inform a matricula do usuario que deseja alocar");
+        for (int j = 0; j < fabricaconcretaU.getUsuariosCadastrados().size()|| achouUsuario==false; j++) {  
+             if (fabricaconcretaU.getUsuariosCadastrados().get(j).getMatricula().equals(matriculaUsuarioAlocar)){
+                 alocacao.setUsuario(matriculaUsuarioAlocar);
+                 achouUsuario = true;
+             } 
+        }
+         if (achouUsuario==false){
+            JOptionPane.showMessageDialog(null, "Matricula do usuario nao encontrado");
+             }
+         
+    //definindoDatas
+        alocacao.setAlocarData(JOptionPane.showInputDialog("Informe o dia que o recurso sera alocado"));
+        
+       
+       // metodo verificar
+       if (achouRecurso==true && achouUsuario==true)
+        for (int k=0; k<alocacoes.size();k++){
+            if (alocacoes.get(k).getRecurso()==alocacao.getRecurso()){
+                if (alocacoes.get(k).getAlocarData()==alocacao.getAlocarData()){
+                    JOptionPane.showMessageDialog(null, "Produto já esta alocado para essa data");
+                }
+                else{
+                    alocacoes.add(alocacao);
+                }
+            }
+ }
+}
+
+    public Alocacao(String recurso, String usuario, String alocarData) {
+        this.recurso = recurso;
+        this.usuario = usuario;
+        this.alocarData = alocarData;
     }
 
-    public Date getDataInicio() {
-        return dataInicio;
+    public static ArrayList<Alocacao> getAlocacoes() {
+        return alocacoes;
     }
 
-    public void setDataInicio(Date dataInicio) {
-        this.dataInicio = dataInicio;
+    public static void setAlocacoes(ArrayList<Alocacao> alocacoes) {
+        Alocacao.alocacoes = alocacoes;
     }
 
-    public Date getDataFim() {
-        return dataFim;
+    public String getRecurso() {
+        return recurso;
     }
 
-    public void setDataFim(Date dataFim) {
-        this.dataFim = dataFim;
+    public void setRecurso(String recurso) {
+        this.recurso = recurso;
     }
 
-    public String getHoraInicio() {
-        return horaInicio;
+    public String getUsuario() {
+        return usuario;
     }
 
-    public void setHoraInicio(String horaInicio) {
-        this.horaInicio = horaInicio;
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
     }
 
-    public String getHoraFim() {
-        return horaFim;
+    public String getAlocarData() {
+        return alocarData;
     }
 
-    public void setHoraFim(String horaFim) {
-        this.horaFim = horaFim;
+    public void setAlocarData(String alocarData) {
+        this.alocarData = alocarData;
     }
+
     
 }
 
