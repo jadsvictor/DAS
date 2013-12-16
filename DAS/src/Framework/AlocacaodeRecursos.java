684fe863ae20f99bef81201abea1a721b3c14d80
Jads <jadsvictor@hotmail.com>
2013-12-16 01:24:02 -0200

    incluirt removeralocacao()... copia e cola da Alocacao()... ta feio pra carai...haha

--- DAS/src/Framework/AlocacaodeRecursos.java
+++ DAS/src/Framework/AlocacaodeRecursos.java
@@ -73,6 +73,60 @@ public class AlocacaodeRecursos {
      
      public static void removerAlocacao(){
      
+        boolean produtoJaAlocado = false;
+    boolean achouRecurso = false;
+    boolean achouUsuario = false ;
+    
+    //pesquisando recurso 
+    
+    AlocacaodeRecursos alocacao = new AlocacaodeRecursos(null,null,null);
+    RecursoFabricaConcreta fabricaconcreta = new RecursoFabricaConcreta();
+    fabricaconcreta.getRecursosCadastrados();
+    alocacao.recurso = JOptionPane.showInputDialog("Informe o numero do patrimonio do recurso que deseja alocar");
+        for (int i = 0; i < fabricaconcreta.getRecursosCadastrados().size(); i++) { 
+             if (fabricaconcreta.getRecursosCadastrados().get(i).numeroPatrimonio.equals(alocacao.recurso)){
+                 achouRecurso = true;
+                 JOptionPane.showMessageDialog(null, "entrou no laco");
+                 break;
+             }
+        }
+        JOptionPane.showMessageDialog(null, "saiu do laco");
+    //pesquisando usuario
+    UsuarioFabricaConcreta fabricaconcretaU = new UsuarioFabricaConcreta();
+    alocacao.usuario = JOptionPane.showInputDialog("Inform a matricula do usuario que deseja alocar");
+        for (int j = 0; j < fabricaconcretaU.getUsuariosCadastrados().size(); j++) {  
+             if (fabricaconcretaU.getUsuariosCadastrados().get(j).getMatricula().equals(alocacao.usuario)){
+                JOptionPane.showMessageDialog(null, "entrou no laco2");                 
+                 achouUsuario = true;
+                 break;
+             } 
+             JOptionPane.showMessageDialog(null, "saiu do laco2");
+        }
+    
+         JOptionPane.showMessageDialog(null, "saiu do usuario");
+    //definindoDatas
+        alocacao.alocarData = JOptionPane.showInputDialog("informar data");
+       JOptionPane.showMessageDialog(null, "seto data");
+       // metodo verificar
+       if (achouRecurso==true && achouUsuario==true){
+           JOptionPane.showMessageDialog(null, "entro no if true");
+        for (int k=0; k<alocacoes.size();k++){
+            JOptionPane.showMessageDialog(null, "entro no for");
+            if (alocacoes.get(k).getRecurso().equals(alocacao.getRecurso())){
+                if (alocacoes.get(k).getAlocarData().equals(alocacao.getAlocarData())){
+                    JOptionPane.showMessageDialog(null, "Produto já esta alocado para essa data");
+                    produtoJaAlocado =true;
+                    int confirma = Integer.parseInt(JOptionPane.showInputDialog("Confirmar exclusao de alocacao? \n1-sim \n2-nao "));
+                    if (confirma==1){
+                    alocacoes.remove(alocacao);
+                    }
+                    else {
+                      break;  
+                    }
+                }
+            }
+        }
+      }  
      
      }
 
